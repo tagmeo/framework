3125ec053ff445a812262b0578f25b56aebabbe7
Kyle Anderson <kyle+tagmeo@divspace.com>
2016-07-21 10:36:39 -0700

    WP_Customize_Manager type hint

--- src/Tagmeo/Admin/Customizer.php
+++ src/Tagmeo/Admin/Customizer.php
@@ -6,14 +6,8 @@ class Customizer
 {
     public function __construct()
     {
-        add_action('customize_register', function ($customizer) {
-            $customizer->remove_section('colors');
-            $customizer->remove_section('header_image');
-            $customizer->remove_section('background_image');
-
+        add_action('customize_register', function (\WP_Customize_Manager $customizer) {
             $customizer->get_setting('blogname')->transport = 'postMessage';
-            $customizer->get_setting('blogdescription')->transport = 'postMessage';
-            $customizer->get_setting('header_textcolor')->transport = 'postMessage';
         });
     }
 }
