82ac876707eced32e7aae3e874e58e6a8132f489
Kyle Anderson <kyle+tagmeo@divspace.com>
2016-07-21 10:38:18 -0700

    Set config array for sidebar registration

--- src/Tagmeo/Theme/Sidebar.php
+++ src/Tagmeo/Theme/Sidebar.php
@@ -7,23 +7,22 @@ class Sidebar
     public function __construct()
     {
         add_action('widgets_init', function () {
-            register_sidebar([
-                'name' => 'Primary',
-                'id' => 'sidebar-primary',
+            $config = [
                 'before_widget' => '<section class="widget %1$s %2$s">',
                 'after_widget' => '</section>',
                 'before_title' => '<h3>',
-                'after_title' => '</h3>',
-            ]);
+                'after_title' => '</h3>'
+            ];
+
+            register_sidebar([
+                'name' => 'Primary',
+                'id' => 'sidebar-primary'
+            ] + $config);
 
             register_sidebar([
                 'name' => 'Footer',
-                'id' => 'sidebar-footer',
-                'before_widget' => '<section class="widget %1$s %2$s">',
-                'after_widget' => '</section>',
-                'before_title' => '<h3>',
-                'after_title' => '</h3>',
-            ]);
+                'id' => 'sidebar-footer'
+            ] + $config);
         });
     }
 
@@ -31,11 +30,8 @@ class Sidebar
     {
         static $display;
 
-        isset($display) || $display = !in_array(true, [
-            is_404(),
-            is_front_page()
-        ]);
+        isset($display) || $display = apply_filters('tagmeo/displaySidebar', true);
 
-        return apply_filters('tagmeo/display_sidebar', $display);
+        return $display;
     }
 }
